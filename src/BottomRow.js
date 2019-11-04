a01aaa38a8893a758852ae9aa27f2a8ae7d554fb
Jake Regner <jregner20@gmail.com>
2019-11-04 16:25:27 -0500

    Add state for Down

--- src/BottomRow.js
+++ src/BottomRow.js
@@ -1,12 +1,13 @@
-import React from "react";
+import React, {useState} from "react";
 import "./App.css";
 
 const BottomRow = () => {
+  const [down, setDown] = useState(1);
   return (
     <div className="bottomRow">
       <div className="down">
         <h3 className="down__title">Down</h3>
-        <div className="down__value">3</div>
+        <div className="down__value">{down}</div>
       </div>
       <div className="toGo">
         <h3 className="toGo__title">To Go</h3>
