67ab52302999a6eaf8f1b30ef9dd3a9a92c3dcbe
Jake Regner <jregner20@gmail.com>
2019-11-04 16:13:29 -0500

    Add click handlers for home/away touchdowns/field goals

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
       <section className="scoreboard">
         <div className="topRow">
           <div className="home">
-            <h2 className="home__name">Lions</h2>
+            <h2 className="home__name">Eagles</h2>
 
             {/* TODO STEP 3 - We need to change the hardcoded values in these divs to accept dynamic values from our state. */}
 
@@ -21,7 +21,7 @@ function App() {
           </div>
           <div className="timer">00:03</div>
           <div className="away">
-            <h2 className="away__name">Tigers</h2>
+            <h2 className="away__name">Packers</h2>
             <div className="away__score">{awScore}</div>
           </div>
         </div>
@@ -30,12 +30,12 @@ function App() {
       <section className="buttons">
         <div className="homeButtons">
           {/* TODO STEP 4 - Now we need to attach our state setter functions to click listeners. */}
-          <button className="homeButtons__touchdown">Home Touchdown</button>
-          <button className="homeButtons__fieldGoal">Home Field Goal</button>
+          <button className="homeButtons__touchdown" onClick= { () => setHoScore(hoScore + 7)}>Home Touchdown</button>
+          <button className="homeButtons__fieldGoal" onClick= { () => setHoScore(hoScore + 3)}>Home Field Goal</button>
         </div>
         <div className="awayButtons">
-          <button className="awayButtons__touchdown">Away Touchdown</button>
-          <button className="awayButtons__fieldGoal">Away Field Goal</button>
+          <button className="awayButtons__touchdown" onClick= { () => setAwScore(awScore + 7)}>Away Touchdown</button>
+          <button className="awayButtons__fieldGoal" onClick= { () => setAwScore(awScore + 3)}>Away Field Goal</button>
         </div>
       </section>
     </div>
